b5b926a2d7bae1b1543cdd5f3f5a4b295b8499af
BrunoProgramer2 <98126497+BrunoProgramer2@users.noreply.github.com>
2022-09-14 15:19:37 -0300

    aña

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 // const url = `https://azure.microsoft.com/${navigator.language}/`
 document.querySelector('html').lang = navigator.language
+const infoAlert = (text,title) => {
+    return new Promise((resolve,reject) => {
+        Swal.fire({
+            title:title,
+            text:text,
+            icon:'info',
+            timer:8000,
+            timerProgressBar:true
+            /*,footer:`<a href="javascript:open('${url}')" style="color:yellow">Anuncio =></a>`*/
+        })
+        resolve()
+    })
+}
 
+const warningAlert = (textError,title) => {
+    return new Promise((resolve,reject) => {
+        Swal.fire({
+            title:title,
+            text:textError,
+            icon:'warning',
+            timer:4000,
+            timerProgressBar:true
+            /*,
+            footer:`<a href="javascript:open('${url}')" style="color:yellow">Anuncio</a>`*/
+        })
+        resolve()
+    })
+}
 let title = `tu escribiste esto
                     ↓`
 const app = new Vue({
